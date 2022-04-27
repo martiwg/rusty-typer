61782a51a97b58fac1fa49307c949db91fafd0d6
martiwg <martiwg.gg@gmail.com>
2022-04-27 18:10:20 +0200

    baby steps

--- main.rs
+++ main.rs
@@ -1,3 +1,9 @@
+use std::io;
+
 fn main(){
-  println!("Hello, world!");
+  println!("Welcome to Rusty Typer!");
+  println!("Press any key to start");
+
+  // need to make this not block the stdin
+  io::stdin().read_line(&mut String::new()).unwrap();
 }
